e81dba3349448fdb9e8b90805523f4ad52700c0e
Dhananjay Singh <dhananjay1204@gmail.com>
2017-11-29 00:54:39 +0530

    Added container

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
+import BooksReducer from './reducer_books';
 
-const rootReducer = combineReducers({
+const rootReducer =combineReducers({
+    books:BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { 'title': 'Javascript : The good parts' },
+        { 'title': 'Angular1' },
+        { 'title': 'Angular2' },
+        { 'title': 'ReactJS' }
+    ]
+}
