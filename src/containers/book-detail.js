c4183165d4f82769a0767f09591b5dd180d29551
Dhananjay Singh <dhananjay1204@gmail.com>
2017-11-30 02:01:04 +0530

    Book detail action

---
+++ src/containers/book-detail.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+
+    render() {
+        if (!this.props.book) {
+            return (
+                <div>
+                    Select a book to start.
+            </div>
+            );
+        }
+
+        return (
+            <div>
+                {this.props.book.title}
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        book: state.activeBook
+    }
+}
+
+export default connect(mapStateToProps)(BookDetail);
+
+
